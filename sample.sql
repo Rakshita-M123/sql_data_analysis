4d7999772921c0851b99e1406033fa419ae23371
Rakshita-M123 <rakshita2233@gmail.com>
2026-03-06 12:06:53 +0530

    Create sample.sql

---
+++ sample.sql
@@ -0,0 +1,15 @@
+INSERT INTO customers VALUES
+(1,'Amit','Delhi'),
+(2,'Sara','Mumbai'),
+(3,'John','Bangalore');
+
+INSERT INTO products VALUES
+(101,'Laptop',60000),
+(102,'Mouse',500),
+(103,'Keyboard',1200);
+
+INSERT INTO orders VALUES
+(1,1,101,1,'2024-01-10'),
+(2,2,102,2,'2024-01-12'),
+(3,3,103,1,'2024-01-13'),
+(4,1,102,3,'2024-01-15');
